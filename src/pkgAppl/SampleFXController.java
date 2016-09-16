2fe1924cce9a4465e8dd25ca6440639a0ff37a82
schueler <schueler@ubuntu>
2016-09-16 03:12:42 -0700

    commit textbox changes

--- src/pkgAppl/SampleFXController.java
+++ src/pkgAppl/SampleFXController.java
@@ -16,7 +16,8 @@ public class SampleFXController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		System.out.println("first thing at first...." + arg0);
-                System.out.println("wfewfewww");
+                System.out.println("ww");
+                
 		
 	}
 
